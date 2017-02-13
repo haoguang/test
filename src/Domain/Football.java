eb963c8a4f2892dac69f37ee25cd662f8d28148e
haoguang <haoguang18@hotmail.com>
2017-02-13 10:06:33 +0800

    added football object to domain

---
+++ src/Domain/Football.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Domain;
+
+/**
+ *
+ * @author haoguang
+ */
+public class Football {
+    private double size;
+    
+    Football(){
+        this(0.0);
+    }
+    
+    Football(double size){
+        this.size = size;
+    }
+}
